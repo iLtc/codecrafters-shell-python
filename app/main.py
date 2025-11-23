4bebca820a3db897e350766818f2fa9c534c65f4
Alan Luo <i@iltc.io>
2025-11-22 22:07:18 -0800

    feat: implement type

--- app/main.py
+++ app/main.py
@@ -1,20 +1,36 @@
 import sys
 
 
+def cmd_echo(params):
+    print(" ".join(params))
+
+
+def cmd_type(params):
+    if params[0] in ['echo', 'exit', 'type']:
+        print(f"{params[0]} is a shell builtin")
+
+    else:
+        print(f"{params[0]}: not found")
+
+
 def main():
     while True:
         sys.stdout.write("$ ")
 
-        command = input()
+        inputs = input().split()
 
-        if command.startswith("echo "):
-            print(command[5:])
-            continue
-
-        if command == "exit":
+        if inputs[0] == "exit":
             break
 
-        print(f"{command}: command not found")
+        if inputs[0] == "echo":
+            cmd_echo(inputs[1:])
+            continue
+
+        if inputs[0] == "type":
+            cmd_type(inputs[1:])
+            continue
+
+        print(f"{inputs[0]}: command not found")
 
 
 if __name__ == "__main__":
